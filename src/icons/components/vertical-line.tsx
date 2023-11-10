c59a09730efb73cfc0d07e4bb88f6c5b0a725d84
vladboyarinov1 <vlad.boyarinov.2002@gmail.com>
2023-11-10 18:09:28 +0300

    feat: add components with svg

---
+++ src/icons/components/vertical-line.tsx
@@ -0,0 +1,30 @@
+import { IconProps, IconWrapper } from '../icon-wrapper'
+
+export const VerticalLine = (allProps: IconProps) => {
+  const { svgProps: props, ...restProps } = allProps
+
+  return (
+    <IconWrapper
+      icon={
+        <svg
+          fill={'none'}
+          height={'100%'}
+          viewBox={'0 0 24 24'}
+          width={'100%'}
+          xmlns={'http://www.w3.org/2000/svg'}
+          {...props}
+        >
+          <line
+            stroke={'currentColor'}
+            strokeLinecap={'round'}
+            x1={'11.8394'}
+            x2={'11.8394'}
+            y1={'22.4707'}
+            y2={'3.4707'}
+          />
+        </svg>
+      }
+      {...restProps}
+    />
+  )
+}

eb662741e7cbcb05bf8574f559f0353afb878a56
vladboyarinov1 <vlad.boyarinov.2002@gmail.com>
2023-11-10 16:46:43 +0300

    feat: add styles

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,9 @@ import { StrictMode } from 'react'
 
 import { createRoot } from 'react-dom/client'
 
-import './index.css'
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/700.css'
+import './styles/index.scss'
 
 import { App } from './App'
 

11f8e308a6234cabce67dcfb603b5f6fb359c3e7
vladboyarinov1 <vlad.boyarinov.2002@gmail.com>
2024-02-10 23:05:39 +0300

    refactor: add changes

--- src/App.tsx
+++ src/App.tsx
@@ -15,13 +15,23 @@ export function App() {
   const isMobile = width && width <= 730
 
   return (
-    <Layout style={{ backgroundColor: 'var(--color-green-desaturated)' }}>
-      <Layout style={{ backgroundColor: 'var(--color-green-desaturated)' }}>
+    <Layout
+      style={{
+        backgroundColor: 'var(--color-green-desaturated)',
+        height: '100vh',
+      }}
+    >
+      <Layout
+        style={{
+          backgroundColor: 'white',
+        }}
+      >
         {isMobile ? <MainHeader /> : <Sidebar />}
         <Layout
           style={{
             backgroundColor: 'var(--color-green-desaturated)',
             marginTop: `${isMobile ? '60px' : 0}`,
+            overflowY: 'auto',
           }}
         >
           <Content
@@ -80,7 +90,68 @@ export function App() {
                         Aliquam autem consectetur deserunt dicta fugiat qui suscipit? A, aliquam
                         atque consequuntur culpa deleniti deserunt excepturi expedita facere fugiat
                         fugit iure laudantium nam nisi nobis nulla quae quas quis sed tempora ullam
-                        voluptatem!
+                        voluptatem! Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ad
+                        architecto beatae ex molestiae saepe! Amet, sint, totam. Ad aut beatae,
+                        cumque debitis eius fugiat laborum magnam maxime, nemo, numquam officiis
+                        omnis quas quis quo sapiente tempore ut voluptate. Asperiores cumque debitis
+                        distinctio ea exercitationem fugiat impedit in magnam magni minus obcaecati
+                        perspiciatis soluta suscipit ullam ut velit, vitae voluptatem? Deserunt
+                        error in laboriosam non. Aliquam autem consequatur, error fuga
+                        necessitatibus numquam qui reprehenderit temporibus veniam vero.
+                        Consequuntur cumque cupiditate deserunt dolores eveniet explicabo id illo,
+                        magnam maiores molestias omnis, perferendis quo quos sunt totam veritatis,
+                        voluptates. Adipisci amet hic nihil repudiandae! Animi atque cupiditate
+                        deleniti inventore nulla obcaecati rerum. Accusamus alias deleniti
+                        doloremque earum maiores, nam odio quos. Atque ducimus in laudantium minima,
+                        molestiae nam quo saepe sed sint suscipit unde veniam? Earum eum fugiat nam
+                        natus neque non quisquam. Accusamus aperiam cum debitis distinctio dolore
+                        doloremque doloribus ex ipsa, ipsum itaque iusto magnam nam odit qui
+                        repellendus sunt voluptatibus! Aliquam assumenda, dolorum incidunt iste
+                        minus quibusdam quidem quos sequi. Accusamus amet atque consequatur debitis
+                        dolor, ducimus eum explicabo harum iure laboriosam, odio quae qui quibusdam
+                        rem totam vel voluptates voluptatibus. Ad beatae consequatur corporis dolor
+                        eveniet in incidunt iure laboriosam libero molestias natus necessitatibus
+                        nisi quam quis quos repellendus, sequi soluta, suscipit vel voluptatem.
+                        Cupiditate eius eos eveniet nam non perspiciatis provident! Ad architecto
+                        debitis doloremque, ducimus, eum fugiat ipsum iste libero magnam minus neque
+                        optio provident ratione totam voluptatem. Aliquam consectetur eaque eligendi
+                        tempora. A blanditiis enim facere quia, recusandae soluta? Alias corporis
+                        deserunt dolorem dolores eos illum impedit mollitia placeat quae ratione?
+                        Architecto aut consectetur distinctio dolore dolorem ducimus eius eligendi
+                        eum, expedita id incidunt, ipsum modi neque nobis odit perferendis placeat
+                        porro possimus, quae quibusdam quis reiciendis repudiandae sequi sit sunt
+                        tempora vel! Amet cum, officia! Lorem ipsum dolor sit amet, consectetur
+                        adipisicing elit. Ab accusantium ad alias architecto, autem consequatur
+                        corporis, dolorem eaque eveniet excepturi expedita explicabo incidunt
+                        laboriosam laudantium maiores modi odio, pariatur porro possimus quam quidem
+                        sunt tenetur vel vero vitae voluptatem voluptatum! Cumque eligendi est illo
+                        in nobis porro sit vero voluptatem? Ab ad aperiam assumenda, blanditiis
+                        consequuntur cumque cupiditate dolor dolorem ducimus illum libero magnam
+                        modi mollitia neque nihil perferendis quae quaerat quasi quia quod
+                        repellendus soluta sunt voluptatem voluptatibus voluptatum? Accusamus beatae
+                        blanditiis corporis cupiditate dolor error fugit ipsum labore laboriosam
+                        libero magni, maxime natus, obcaecati odio officia pariatur praesentium quae
+                        quam quas quidem quisquam repellendus sapiente similique temporibus unde
+                        voluptate voluptates voluptatibus? Eum maiores sapiente sunt. A ab accusamus
+                        aperiam aspernatur autem blanditiis cupiditate delectus dignissimos
+                        distinctio dolor dolorum eius error excepturi impedit incidunt ipsam ipsum
+                        laboriosam laborum magnam magni mollitia natus odio officiis quam, qui quia
+                        quidem quisquam quo rem repellendus sequi sint temporibus totam ullam vero
+                        voluptas voluptatibus. Ad aliquid consequatur, deleniti doloribus ducimus ex
+                        mollitia possimus rem sapiente sit ut voluptate voluptatem! A ad amet
+                        consectetur culpa deserunt dicta, dolores eius error fuga fugiat fugit illo
+                        iure maiores, natus neque non nulla obcaecati odit officiis optio
+                        perferendis perspiciatis placeat quidem recusandae tenetur totam
+                        voluptatibus! A culpa esse neque non quod sequi, veniam! Accusamus beatae
+                        eaque laboriosam officia sint! Accusantium atque doloremque, dolorum eaque
+                        eveniet exercitationem fugiat fugit nobis numquam sequi, similique tenetur
+                        unde voluptatibus! A accusamus alias aliquid autem commodi culpa delectus
+                        dicta dolore dolorem doloremque dolorum ea eaque eos eum ipsum iste itaque
+                        iure iusto, laborum magni maiores maxime molestias nemo nesciunt nostrum
+                        perspiciatis porro possimus praesentium quas quis quisquam quos ratione
+                        reprehenderit rerum tenetur ullam voluptate? A aperiam commodi corporis
+                        error exercitationem id libero non quia sed vitae? Blanditiis enim laborum
+                        quas saepe voluptas?
                       </div>
                     </div>
                   }

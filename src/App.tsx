97d05717ff9212add819f6959cd6b38c9b16e6e8
vladboyarinov1 <vlad.boyarinov.2002@gmail.com>
2024-02-19 21:29:19 +0300

    feat: started the 'about search' component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, useLocation } from 'react-router-dom'
 
+import { AboutSearch } from '@/components/ui/about-search'
 import { LineGraph } from '@/components/ui/diagrams/line-graph'
 import { MainHeader } from '@/components/ui/header/header'
 import { Sidebar } from '@/components/ui/sidebar'
@@ -12,6 +13,12 @@ import { Content } from 'antd/es/layout/layout'
 export function App() {
   const { selectedKeys } = useBreadcrumbs()
   const { width } = useWindowSize()
+  const location = useLocation()
+
+  // Теперь у вас есть доступ к location.pathname
+  const currentPath = location.pathname
+
+  console.log(currentPath)
   const isMobile = width && width <= 730
 
   return (
@@ -26,11 +33,11 @@ export function App() {
           backgroundColor: 'white',
         }}
       >
-        {isMobile ? <MainHeader /> : <Sidebar />}
+        {currentPath !== '/about_search' ? isMobile ? <MainHeader /> : <Sidebar /> : ''}
         <Layout
           style={{
             backgroundColor: 'var(--color-green-desaturated)',
-            marginTop: `${isMobile ? '60px' : 0}`,
+            marginTop: `${isMobile && currentPath !== '/about_search' ? '60px' : 0}`,
             overflowY: 'auto',
           }}
         >
@@ -39,23 +46,28 @@ export function App() {
               margin: '0 20px',
             }}
           >
-            <Breadcrumb
-              items={selectedKeys.map(i => ({ title: i }))}
-              separator={'>'}
-              style={{
-                fontSize: 16,
-                margin: '40px 0 20px 20px',
-              }}
-            />
+            {currentPath !== '/about_search' ? (
+              <Breadcrumb
+                items={selectedKeys.map(i => ({ title: i }))}
+                separator={'>'}
+                style={{
+                  fontSize: 16,
+                  margin: '40px 0 20px 20px',
+                }}
+              />
+            ) : (
+              ''
+            )}
             <div
-              style={{
-                background: 'white',
-                borderRadius: 16,
-                minHeight: 360,
-                padding: 21,
-              }}
+            // style={{
+            //   background: 'white',
+            //   borderRadius: 16,
+            //   minHeight: 360,
+            //   padding: 21,
+            // }}
             >
               <Routes>
+                <Route element={<AboutSearch />} path={'/about_search'} />
                 <Route
                   element={
                     <div>
---
+++ src/components/ui/about-search/cards/cards.tsx
@@ -0,0 +1,52 @@
+import { Link } from 'react-router-dom'
+
+import { AnalyticsChart, AnalyticsGraph, Search } from '@/icons'
+
+import s from './cards.module.scss'
+const cards = [
+  {
+    id: 1,
+    img: <Search color={'white'} size={20} />,
+    path: '/path1',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    title: 'Поиск данных',
+  },
+  {
+    id: 2,
+    img: <AnalyticsGraph color={'white'} size={20} />,
+    path: '/path2',
+    subtitle: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    title: 'Аналитика',
+  },
+  {
+    id: 3,
+    img: <AnalyticsChart color={'white'} size={20} />,
+    path: '/path3',
+    subtitle:
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' +
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit.' +
+      'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+    title: 'Конфиденциальность',
+  },
+]
+
+export const Cards = () => {
+  return (
+    <>
+      {cards.map(card => (
+        <div className={s.container} key={card.id}>
+          <div className={s.img}>{card.img}</div>
+          <div className={s.title}>
+            <h3>{card.title}</h3>
+          </div>
+          <div className={s.subtitle}>
+            <p>{card.subtitle}</p>
+          </div>
+          <div className={s.link}>
+            <Link to={card.path}>Перейти</Link>
+          </div>
+        </div>
+      ))}
+    </>
+  )
+}
